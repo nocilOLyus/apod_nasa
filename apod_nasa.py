41fb933afbd7399c63fec17378e376d7dda3e816
nocilOLyus <62856857+nocilOLyus@users.noreply.github.com>
2021-12-08 02:29:23 +0100

    cleaned up console output and added size
    so now the output is just the name of the file and its size

--- apod_nasa.py
+++ apod_nasa.py
@@ -1,9 +1,9 @@
 import subprocess
 import requests
 
-base_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"       # Replace DEMO_KEY with your api key.
-parameters = "&count=1&thumbs=true"                                     # (because of the count=1, we get a single dictionary in a list,
-                                                                        # but it's necessary to get a random image everytime.)
+base_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"       # Replace DEMO_KEY by your api key.
+parameters = "&count=1&thumbs=true"                                     # because of the count=1, we get a single dictionary in a list,
+                                                                        # but it's necessary to get a random image everytime.
 media = filename = image_url = ""
 while True:
     while media != "image":
@@ -22,14 +22,17 @@ while True:
 
 title = apod_json[0]["title"]
 for char in title:
-    if char in '<>:"/\|?*\r\n\t ':
+    if char in '<>:"/\|?*\r\n ':
         filename += "_"
     elif char not in ",.":
         filename += char
 filename += ".jpg"
-print(f"{apod_json}\n{filename}")
+#print(apod_json)
+print(filename)
 
 image = requests.get(image_url)
+image_size = int(image.headers["Content-Length"])
+print(f"Size: {round(image_size / 1000, 2)} kB\t{round(image_size / 1_000_000, 2)} MB")
 with open(f"images\\{filename}", "wb") as f:
     f.write(image.content)
 
